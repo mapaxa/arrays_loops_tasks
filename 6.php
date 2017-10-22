0b2e563c69eeeb5a48de64eaf79c05db2f3d4e19
mapaxa <alexeybespalov86@gmail.com>
2017-10-22 16:13:22 +0300

    6

--- 6.php
+++ 6.php
@@ -1,3 +1,15 @@
 <?php
+//6. Дан массив $arr. С помощью цикла foreach запишите английские названия в массив $en, а русские — в массив $ru. $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой'); $en = array('green', 'red','blue'); $ru = array('зеленый', 'красный', 'голубой');
+
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+
+$en = array();
+$ru = array();
+
+foreach ($arr as $key => $value) {
+	$en[]	= $key;
+	$ru[] = $value;
+}
+
 
  ?>
